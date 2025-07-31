43e927b2149f7ca36af2fcee9e237f27f144517a
yotan660 <yotilahun@aol.com>
2025-07-31 10:56:42 -0400

    added the welcome font

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
+print("Welcome to BMI Calculator")
 weight=float(input("Enter your weight in (kgs):"))
 height=float(input("Enter your height in (m)): "))
 bmi=(weight)/(height**2)
 print(bmi)
+
 
 
